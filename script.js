c5285f84a52b1caa6f9ed424025af1182d59186f
angel0060 <angelica.535230060@stu.untar.ac.id>
2024-10-26 23:04:00 +0700

    Membuat halaman contact, Mengubah form subscribe supaya tervalidate required terisi semua baru muncul modal, ganti style input & text area

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ function setActive(event) {
  
     event.currentTarget.classList.add('active');
 }
+
+function validateSubscribe() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+
+    if (name && email) {
+        const modal = new bootstrap.Modal(document.getElementById('modalpress'));
+        modal.show();
+    } else {
+        alert('Please fill in all fields before sending your message.');
+    }
+}
+
+function validateContact() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    if (name && email && message) {
+        const modal = new bootstrap.Modal(document.getElementById('modalpress'));
+        modal.show();
+    } else {
+        alert('Please fill in all fields before sending your message.');
+    }
+}
